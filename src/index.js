963f2a00e0c3394b314503a1577503821dd52261
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-16 14:50:28 +0600

    Fix

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import reducer from './reducer';
+
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter> ,
   document.getElementById("root")
 );
