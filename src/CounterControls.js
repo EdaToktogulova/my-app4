f66f837a95e3dc000f88e305eac5a2052bc9fcf7
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-16 15:00:34 +0600

    news

--- src/CounterControls.js
+++ src/CounterControls.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import { useState } from "react";
 
 function CounterControls() {
+   const [counter, setCounter] = useState(0);
+
+   function onIncrement() {
+      setCounter(counter + 1);
+   }
+   function onDecrement() {
+      setCounter(counter - 1);
+   }
+   function onReset() {
+      setCounter(0);
+   }
    return (
-      <div className='CounterControls'>
-         <button ></button>
-         <button></button>
+      <div>
+         <button onClick={onIncrement}>+</button>
+         <button onClick={onDecrement}>-</button>
+         <button onClick={onReset}>Reset</button>
       </div>
-   )
+   );
 }
 
-export default CounterControls
+export default CounterControls;
